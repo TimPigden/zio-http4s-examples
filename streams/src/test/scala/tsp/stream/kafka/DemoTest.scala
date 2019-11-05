d5e46e2657ae05b3536d2c98b0bf1c79da0c59a8
tim <tim.pigden@optrak.com>
2019-11-05 15:01:54 +0000

    updated to RC16, changed Schedule.spaced to ZSchedule.spaced

--- streams/src/test/scala/tsp/stream/kafka/DemoTest.scala
+++ streams/src/test/scala/tsp/stream/kafka/DemoTest.scala
@@ -5,12 +5,23 @@ import zio.test._
 import zio.test.Assertion._
 import zio._, zio.duration._
 import zio.kafka.client._
+import zio.kafka.client.serde._
 
 object DemoTest extends DefaultRunnableSpec (
 
-  suite("test emitting stream")(
-    testM("random walk"){
-
+  suite("test basic kafka stuff")(
+    testM("test connection"){
+      val settings: ConsumerSettings =
+        ConsumerSettings(
+          bootstrapServers          = List("localhost:9092"),
+          groupId                   = "group",
+          clientId                  = "client",
+          closeTimeout              = 30.seconds,
+          extraDriverSettings       = Map(),
+          pollInterval              = 250.millis,
+          pollTimeout               = 50.millis,
+          perPartitionChunkPrefetch = 2
+        )
 
       IO.succeed(assert(true, equalTo(true)))
     }
